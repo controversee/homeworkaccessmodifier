fde8bc1fa6bef77403ab930cdf430a9d8d130425
Nigar <nigarks@code.edu.az>
2022-12-26 12:54:15 +0400

    new homework was added

---
+++ homeworkaccessmodifiers/homeworkaccessmodifiers/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace homeworkaccessmodifiers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1. User əlavə et");
+            Console.WriteLine("2. Userlara bax");
+            Console.WriteLine("3. Userlar üzrə axtarış et");
+            string str1 = Console.ReadLine();
+            User[] userArr = new User[0];
+            User Nigar = new User(null);
+            if (str1 == "1")
+            {
+                Console.WriteLine("Username-i daxil edin!");
+                Nigar.Username = Console.ReadLine();
+                Console.WriteLine("Password-u daxil edin!");
+                Nigar.Password = Console.ReadLine();
+
+                Array.Resize(ref userArr, userArr.Length + 1);
+
+                userArr[userArr.Length - 1] = Nigar;
+            }
+            else if (str1 == "2")
+            {
+                for (int i=0; i<userArr.Length; i++)
+                {
+                    Console.WriteLine(userArr[i].Username); 
+                }
+            }
+
+
+        }
+
+    }
+}
---
+++ homeworkaccessmodifiers/homeworkaccessmodifiers/User.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace homeworkaccessmodifiers
+{
+    internal class User
+    {
+        private string _username;
+        private string _password;
+        public User(string username)
+        {
+            
+           
+        }
+        public string Username
+        {
+            get => _username;
+            set
+            {
+                if (value.Length >= 6 && value.Length <= 25)
+                    _username = value;
+
+            }
+             
+            
+        }
+        public string Password
+        {
+            get => _password;
+            set
+            {
+                if (!string.IsNullOrWhiteSpace(value))
+                {
+
+                    if (HasDigit(value)&& HasUpper(value)&& HasLower(value)&& value.Length>=8&& value.Length<=25 )
+                    {
+                        _password = value;
+
+
+                    }
+                }
+                else
+                    Console.WriteLine("Passwordu duzgun daxil edin!");
+
+
+
+            }
+
+        }
+        public bool HasDigit(string str)
+        {
+            for (int i=0; i<str.Length; i++)
+            {
+                if (char.IsDigit(str[i]))
+                {
+                    return true;
+
+                }
+            }
+            Console.WriteLine("Reqem daxil edin!");
+            return false;
+
+
+        }
+        public bool HasUpper(string str)
+        {
+            for (int i=0; i<str.Length; i++)
+            {
+                if (char.IsUpper(str[i]))
+                {
+                    return true;
+                }
+            }
+            Console.WriteLine("Min. 1 Boyuk herf daxil edin!");
+            return false;
+
+        }
+        public bool HasLower(string str)
+        {
+            for (int i=0; i<str.Length; i++)
+            {
+                if (char.IsLower(str[i]))
+                {
+                    return true;
+
+                }
+            }
+            Console.WriteLine("Min. 1 Kichik herf daxil edin!");
+            return false;
+
+        }
+
+
+        
+}
+}
